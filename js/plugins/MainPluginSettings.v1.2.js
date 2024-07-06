e288f11a1d9834b7db7d832edb8449ab4a0e6491
mvrec <101111405+mvrec@users.noreply.github.com>
2024-07-06 16:20:29 +0530

    update

--- js/plugins/MainPluginSettings.v1.2.js
+++ js/plugins/MainPluginSettings.v1.2.js
@@ -102,9 +102,38 @@
         });
     }
 
-    // :: 4.0
+    // :: 4.0 Popup Single Image
+    if ($.fn.magnificPopup) {
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+          verticalFit: true
+        }
+      });
+    }  
 
-    // :: 5.0
+    // :: 5.0 Popup Image Gallery
+    if ($.fn.magnificPopup) {
+      $(".popup-gallery").magnificPopup({
+        delegate: "a",
+        type: "image",
+        tLoading: "Loading image #%curr%...",
+        mainClass: "mfp-img-mobile",
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+          titleSrc: function (item) {
+            return item.el.attr("title") + " by Mix Vibe Records";
+          },
+        },
+      });
+    }
 
     // :: 6.0
 
